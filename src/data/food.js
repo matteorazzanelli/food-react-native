4ddd098a7ffd5c353be28ea71267558d038dc131
matteo <matteo.razzanelli89@gmail.com>
2024-03-18 10:46:31 +0100

    feat: add react-query functionality example

--- src/data/food.js
+++ src/data/food.js
@@ -11,7 +11,7 @@ const foodItems = [
         id: '0',
         title: 'Pane',
         pickup: '19:30-20:00',
-        uri: '807/680/957/7465/front_it.35.400.jpg',
+        uri: `${baseUrl}807/680/957/7465/front_it.35.400.jpg`,
         price: '0€',
         remaining: '10',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -20,7 +20,7 @@ const foodItems = [
         id: '1',
         title: 'Pasta',
         pickup: '19:30-20:00',
-        uri: '807/680/208/5738/front_it.3333.400.jpg',
+        uri: `${baseUrl}807/680/208/5738/front_it.3333.400.jpg`,
         price: '0€',
         remaining: '10',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -29,7 +29,7 @@ const foodItems = [
         id: '2',
         title: 'Biscotti',
         pickup: '00:00-23:59',
-        uri: '801/335/599/9143/front_it.49.400.jpg',
+        uri: `${baseUrl}801/335/599/9143/front_it.49.400.jpg`,
         price: '0€',
         remaining: '10',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -46,7 +46,7 @@ const foodItems = [
         id: '0',
         title: 'Pizza',
         pickup: '10:00-12:00',
-        uri: '801/567/302/9595/front_it.3.400.jpg',
+        uri: `${baseUrl}801/567/302/9595/front_it.3.400.jpg`,
         price: '3€',
         remaining: '1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -55,7 +55,7 @@ const foodItems = [
         id: '1',
         title: 'Piadine',
         pickup: '12:00-14:00',
-        uri: '800/778/050/1354/front_it.14.400.jpg',
+        uri: `${baseUrl}800/778/050/1354/front_it.14.400.jpg`,
         price: '1€',
         remaining: '2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -72,7 +72,7 @@ const foodItems = [
         id: '0',
         title: 'Tagliata di Pollo',
         pickup: '12:00-14:00',
-        uri: '405/648/909/7785/front_it.3.400.jpg',
+        uri: `${baseUrl}405/648/909/7785/front_it.3.400.jpg`,
         price: '5€',
         remaining: '2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -81,7 +81,7 @@ const foodItems = [
         id: '1',
         title: 'Arista di Maiale',
         pickup: '20:00-22:00',
-        uri: '206/003/300/3490/front_it.4.400.jpg',
+        uri: `${baseUrl}206/003/300/3490/front_it.4.400.jpg`,
         price: '4€',
         remaining: '1',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -90,7 +90,7 @@ const foodItems = [
         id: '2',
         title: 'Tonno in scatola',
         pickup: '00:00-23:59',
-        uri: '800/403/038/1681/front_it.8.400.jpg',
+        uri: `${baseUrl}800/403/038/1681/front_it.8.400.jpg`,
         price: '2€',
         remaining: '5',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -99,7 +99,7 @@ const foodItems = [
         id: '3',
         title: 'Manzo',
         pickup: '12:00-13:00',
-        uri: '405/648/909/8249/front_it.3.400.jpg',
+        uri: `${baseUrl}405/648/909/8249/front_it.3.400.jpg`,
         price: '5€',
         remaining: '2',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -116,7 +116,7 @@ const foodItems = [
         id: '0',
         title: 'Insalata',
         pickup: '10:00-14:00',
-        uri: '20823931/front_it.8.400.jpg',
+        uri: `${baseUrl}20823931/front_it.8.400.jpg`,
         price: '2€',
         remaining: '5',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -125,7 +125,7 @@ const foodItems = [
         id: '1',
         title: 'Humus di ceci',
         pickup: '06:00-14:00',
-        uri: '405/809/430/0502/front_it.50.400.jpg',
+        uri: `${baseUrl}405/809/430/0502/front_it.50.400.jpg`,
         price: '1€',
         remaining: '5',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -134,7 +134,7 @@ const foodItems = [
         id: '2',
         title: 'Riso alle verdure',
         pickup: '10:00-14:00',
-        uri: '800/186/018/6691/front_it.3.400.jpg',
+        uri: `${baseUrl}800/186/018/6691/front_it.3.400.jpg`,
         price: '3€',
         remaining: '3',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
--- src/screens/Details.js
+++ src/screens/Details.js
@@ -3,19 +3,44 @@ import React from 'react'
 
 import foodItems from '../data/food';
 
+import {
+  useQuery,
+} from '@tanstack/react-query'
+import axios from 'axios'
+
 export default function Details({ route, navigation }) {
+
   const {itemId, sectionId} = route.params;
   const product = foodItems[sectionId].data[itemId]
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>{product.title}</Text>
-      <Image
-          source={{uri: product.uri}}
-          style={styles.itemPhoto}
-        />
-      <Text>Description: {product.description}</Text>
-    </View>
-  )
+
+  const { isPending, error, data, isSuccess } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => {
+      const comment = sectionId*itemId+1; // FIXME: is only a sample
+      return axios.get(
+      `https://jsonplaceholder.typicode.com/comments/${comment}`
+      ).then((res) => res.data)
+    }
+  })
+
+  if (isPending) return <View><Text>'Loading...'</Text></View>
+
+  if (error) return <View><Text>'An error has occurred: ' + error.message</Text></View>
+
+  if(isSuccess)
+    return (
+      <>
+        <View style={styles.container}>
+          <Text style={styles.header}>{product.title}</Text>
+          <Image
+              source={{uri: product.uri}}
+              style={styles.itemPhoto}
+            />
+          {/* <Text>Description: {product.description}</Text> */}
+          <Text>👀 Description: {data.body}</Text>
+        </View>
+      </>
+    )
 }
 
 const styles = StyleSheet.create({
